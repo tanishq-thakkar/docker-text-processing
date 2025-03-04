2bd5a89d4f34d89c72b502b564ca7cbf27eec307
Tanishq Thakkar <tanishq@Tanishqs-MBP.lan>
2025-03-04 18:16:07 -0500

    updates to script

--- script.py
+++ script.py
@@ -23,7 +23,23 @@ def process_text(filename, handle_contractions=False):
         words = text.split()
 
         if handle_contractions:
-            words = [word if "'" not in word else word.split("'")[0] for word in words]
+            contractions = {
+                "i'm": "i am", "you're": "you are", "he's": "he is", "she's": "she is", 
+                "it's": "it is", "we're": "we are", "they're": "they are", "can't": "can not",
+                "won't": "will not", "don't": "do not", "doesn't": "does not", "didn't": "did not",
+                "isn't": "is not", "aren't": "are not", "wasn't": "was not", "weren't": "were not",
+                "haven't": "have not", "hasn't": "has not", "hadn't": "had not", "shouldn't": "should not",
+                "wouldn't": "would not", "couldn't": "could not", "mustn't": "must not", "shan't": "shall not"
+            }
+
+            expanded_words = []
+            for word in words:
+                if word in contractions:
+                    expanded_words.extend(contractions[word].split())  
+                else:
+                    expanded_words.append(word)
+
+            words = expanded_words
 
         return words
 
@@ -34,7 +50,9 @@ for name, path in input_files.items():
     is_contractions_handling_needed = "AlwaysRememberUsThisWay" in name
     words = process_text(path, handle_contractions=is_contractions_handling_needed)
     word_counts[name] = len(words)
-    top_words[name] = Counter(words).most_common(3)
+
+    if "IF-1.txt" in name:
+        top_words[name] = Counter(words).most_common(3)
 
 grand_total = sum(word_counts.values())
 
@@ -47,7 +65,6 @@ with open(output_file, "w") as f:
     f.write(f"Total words in {input_files['AlwaysRememberUsThisWay-1.txt']}: {word_counts['AlwaysRememberUsThisWay-1.txt']}\n")
     f.write(f"Grand Total words: {grand_total}\n")
     f.write(f"Top 3 words in IF-1.txt: {top_words['IF-1.txt']}\n")
-    f.write(f"Top 3 words in AlwaysRememberUsThisWay-1.txt: {top_words['AlwaysRememberUsThisWay-1.txt']}\n")
     f.write(f"Container IP Address: {ip_address}\n")
 
 with open(output_file, "r") as f:
